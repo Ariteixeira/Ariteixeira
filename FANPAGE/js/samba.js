bbcda3d40fe40e8d856a754c04f61baebe813f8e
Ariteixeira <48811854+Ariteixeira@users.noreply.github.com>
2019-04-10 18:52:05 -0300

    Update samba.js

--- FANPAGE/js/samba.js
+++ FANPAGE/js/samba.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+
+
+  $( "#menu-esquerdo" ).height($( "#menu-central" ).height());
+  $( "#menu-direito" ).height($( "#menu-central" ).height());
+
+   
+   	 
+  $("#bt2").click(function(){
+    $("#teste").hide();
+	 $( "#menu-esquerdo" ).height($( "#menu-central" ).height());
+  $( "#menu-direito" ).height($( "#menu-central" ).height());
+  });
+  $("#bt1").click(function(){
+    $("#teste").show();
+	 $( "#menu-esquerdo" ).height($( "#menu-central" ).height());
+  $( "#menu-direito" ).height($( "#menu-central" ).height());
+    });
+});
 
